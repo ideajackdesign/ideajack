079c33eaf9a543d7c3af49636399e74820876f69
oishi-riku <soukei.creator@gmail.com>
2021-12-14 19:53:25 +0900

    Fix: about詳細ページのSSG対応

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
 
 import About from 'components/pages/about/';
+import { About as AboutType, AboutItem } from 'domains/microCMS/models/about';
+import { getAbout } from 'domains/microCMS/services/about';
 
-const Index: NextPage = () => {
-  return <About />;
+type StaticProps = { about: AboutType; profile: AboutItem | undefined };
+
+const Index: NextPage<StaticProps> = ({ about, profile }) => {
+  return <About about={about} profile={profile} />;
 };
 
 export default Index;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const a = await getAbout();
+  const about = {
+    ...a,
+    contents: a.contents.filter((_a) => _a.title !== 'profile'),
+  };
+  const profile = a.contents.find((_a) => _a.title === 'profile');
+
+  return {
+    props: {
+      about,
+      profile,
+    },
+    revalidate: 10,
+  };
+};
